06107128fab25f781c65f42718e8a4337b23e90c
cclamb <cclamb@ece.unm.edu>
2013-05-22 12:42:25 -0600

    fleshed out example

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
-int c_entry(void) {
-    return 0;
+// The UART address associated with QEMU.
+// The unsigned int type forces 32-bit reads and writes.
+volatile unsigned int* const UART0DR = (unsigned int*) 0x101f1000;
+
+void static print_uart0(const char* s) {
+    while (*s != '\0') {
+        *UART0DR = (unsigned int)(*s);
+        s++;
+    }
 }
+
+void c_entry(void) {
+    print_uart0("Hello ARM!\n");
+}
+
+
